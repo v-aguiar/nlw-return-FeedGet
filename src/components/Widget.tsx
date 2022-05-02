6b951a4e6c6a7c1726dd9ff59e912187ad068b5d
v-aguiar <v.aguiar.dev@gmail.com>
2022-05-02 15:03:29 -0300

    Initialize React-Typescript app with Vite. Create initial widget component file

---
+++ src/components/Widget.tsx
@@ -0,0 +1,15 @@
+﻿import { ChatTeardropDots } from "phosphor-react"
+import { Popover } from '@headlessui/react'
+
+export function Widget() {
+  return (
+    <Popover className="absolute bottom-6 right-6">
+      <Popover.Panel><img className="w-15 mb-5" src="https://acegif.com/wp-content/gifs/dancing-cat-43.gif"></img></Popover.Panel>
+
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6" />
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">Feedback</span>
+      </Popover.Button>
+    </Popover>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#8257E5",
+          300: "#996DFF",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
